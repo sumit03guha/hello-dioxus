fc1a271f49159d61a8e97f2d2e018801d345a3f4
Sumit Guha <sumit03guha@gmail.com>
2025-05-07 00:01:41 +0530

    Add color logic to ConditionalDiv component for dynamic styling

--- src/main.rs
+++ src/main.rs
@@ -89,5 +89,7 @@ fn DisplayCounter() -> Element {
 #[component]
 fn ConditionalDiv() -> Element {
     let is_hidden = false;
-    rsx!(div { hidden: is_hidden, "This Div can be hidden" })
+    let is_blue_colour = false;
+
+    rsx!(div { hidden: is_hidden, color: if is_blue_colour { "blue" } else { "green" }, "This Div can be hidden" })
 }
